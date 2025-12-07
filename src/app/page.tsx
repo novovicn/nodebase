de7e12d5efcc29fac06e7de27e674e8766aba4eb
novovicn <novovicnenad96@gmail.com>
2025-12-07 20:33:09 +0100

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
   return(
     <div className="text-red-500 font-bold">
       Hello World
+      {JSON.stringify(users)}
       <Button>Click me</Button>
     </div>
   )
